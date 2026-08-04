7181618959a6ad4c0b73d3a83833c8fe3b2f8a35
Your Name <you@example.com>
2026-08-04 09:39:58 +0300

    refactor: remove checkout method from PaymentController

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,25 +1,3 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Enums\EnrollmentStatus;
-use App\Models\Course;
-use App\Models\Enrollment;
-use App\Models\Payment;
-use App\Services\FawryPaymentService;
-use Illuminate\Http\Request;
-use Throwable;
-
-class PaymentController extends Controller
-{
-    public function checkout(Request $request, Course $course)
-    {
-        abort_unless($course->is_published, 404);
-        abort_if($request->user()->hasActiveSubscriptionTo($course), 403);
-
-        return view('payments.checkout', compact('course'));
-    }
-
     public function pay(Request $request, Course $course, FawryPaymentService $fawry)
     {
         abort_unless($course->
